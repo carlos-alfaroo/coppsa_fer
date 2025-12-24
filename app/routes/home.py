20147833ea6fb592b5c74feb89b4221901514259
Carlos Alfaro <carlosb.alfaro04@gmail.com>
2025-12-24 15:09:28 -0500

    Arreglar detallitos y mejoras

--- app/routes/home.py
+++ app/routes/home.py
@@ -5,3 +5,15 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/')
 def home():
   return render_template('home.html')
+
+@home_bp.route('/pozos')
+def pozos():
+  return render_template('pozos.html')
+
+@home_bp.route('/fosas')
+def fosas():
+  return render_template('fosas.html')
+
+@home_bp.route('/banos')
+def banos():
+  return render_template('banos.html')
